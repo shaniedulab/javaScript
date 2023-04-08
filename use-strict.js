027aed7add8d6dbe9d21568ed0fd1097728432db
shaniedulab <117700394+shaniedulab@users.noreply.github.com>
2023-04-08 11:07:04 +0530

    Create use-strict.js

---
+++ use-strict.js
@@ -0,0 +1,30 @@
+'use strict'
+
+// isTruetype1 is not defined
+
+// a=0
+
+// isTruetype1 is not defined
+
+// let isTruetype = false;
+// let isTruetype1=true;
+
+// if(isTruetype1){
+//     isTruetype1=true;
+// }
+// console.log(isTruetype);
+
+
+// Duplicate parameter name not allowed in this context
+
+// function fun1(name,name){
+//     console.log(name,name);
+// }
+
+// fun1('shan','man')
+
+// Unexpected strict mode reserved word
+
+// let interface = 0
+
+//console.table([{ a: 1, b: 'Y' }, { a: 'Z', b: 2 },{ a: 'Z', b: 2 }],['a']);
